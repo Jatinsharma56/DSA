469295564a9228724258c13ab93fad9895f2049e
Jatin Sharma <jatinsharma00877@gmail.com>
2026-07-21 11:00:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l=0;
+        int r=height.length-1;
+        int max=0;
+        while(l<r){
+            int width=r-l;
+            int area=Math.min(height[l],height[r]*width);
+                max=Math.max(max,area);
+                if(height[l]<height[r]){
+                    l++;
+                }else{
+                    r--;
+                }
+            
+        }
+        return max;
+        
+    }
+}
